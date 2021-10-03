41c39447a250d59efd2c4e36cd338786ed82061b
Diego Taveras <diegotaveras2@outlook.com>
2021-10-03 11:28:08 -0400

    small card component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,45 @@
 import Head from 'next/head'
 import Header from './../components/Header';
 import Banner from './../components/Banner';
+import SmallCard from './../components/SmallCard';
 
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
       <Banner />
-      
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({img, distance, location})=>(
+              <SmallCard key={img} img={img} distance={distance} location={location} />
+            ))}
+          </div>
+        </section>
+      </main>
+
     </div>
   )
+}
+
+export async function getStaticProps(){
+
+  const exploreData = await fetch('https://links.papareact.com/pyp').
+  then((
+    (res) => res.json()
+  ))
+
+  return {
+    props:{
+      exploreData
+    }
+  }
 }
